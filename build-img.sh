d7cdf926972d1b988b3f8c8f5740f8fdcccd37b2
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-06-06 17:28:46 +0100

    Update script to support multiple distros

--- build-img.sh
+++ build-img.sh
@@ -2,11 +2,16 @@
 set -e
 
 # check the parameters
-if [ "$#" -ne 1 ]; then
-    echo "Usage: ./build-img.sh <tag>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./build-img.sh <tag> <distro>"
     exit 1
 fi
 
 # build the image
+distro=$2
 constellation_tag=$1
-docker build -t constellation:${constellation_tag} --build-arg ledger_tag=${constellation_tag} .
+docker build \
+	-t constellation:${constellation_tag} \
+	--build-arg ledger_tag=${constellation_tag} \
+	-f ${distro}/Dockerfile \
+	.
